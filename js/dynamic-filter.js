343f0e4a48f649c039777651acbb2c3b1dee4123
Matt <m_dittmann@yahoo.com>
2017-06-11 23:24:24 -0700

    update js

--- js/dynamic-filter.js
+++ js/dynamic-filter.js
@@ -42,4 +42,33 @@
     $table.append($tbody);
   }
 
+  function update(min, max) {
+    rows.forEach(function(row) {
+      if (row.person.rate >= min && row.person.rate <= max) {
+        row.$element.show();
+      } else {
+        row.$element.hide();
+      }
+    });
+  }
+
+  function init() {
+    $('#slider').noUiSlider({
+      range:[0, 150],
+      start: [65, 90],
+      handles: 2,
+      margin: 20,
+      connect: true,
+      serialization: {
+        to: [$min, $max],
+        resolution: 1
+      });
+    }).change(function() {
+      update($min.val(), $max.val());
+    });
+    makeRows();
+    appendRows();
+    update($min.val(), $max.val());
+  }
+$(init);
 }());
